88af45eaad919dae0db4eaa803c7d3078c6f7a4d
jungai <juworaphol125@gmail.com>
2021-05-01 16:50:42 +0700

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,15 @@ module.exports = {
   rules: {
     "no-use-before-define": "off",
     "import/no-extraneous-dependencies": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
   },
+  overrides: [
+    {
+      files: ["src/**/*.ts"],
+      rules: {
+        "import/prefer-default-export": 0,
+        "no-shadow": "off",
+      },
+    },
+  ],
 };
