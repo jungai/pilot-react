9270cba71534330df960549934b1bd2f020323c9
jungai <juworaphol125@gmail.com>
2021-05-02 12:56:42 +0700

    add appointment page

---
+++ src/pages/appointment/annual-services/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const AnnualServicesPage: React.FC = () => <div>AnnualServicesPage</div>;
+
+export default AnnualServicesPage;
---
+++ src/pages/appointment/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const AppointmentPage: React.FC = () => <div>AppointmentPage</div>;
+
+export default AppointmentPage;
